bfa02c9bc2297e7a15bd5b5ccf01f53a2335bed9
shashwatverma14@gmail.com <shashwatverma14@gmail.com>
2019-06-22 18:22:46 -0400

    add performance metrics

--- unified_pose_estimation/test.py
+++ unified_pose_estimation/test.py
@@ -8,7 +8,7 @@ from net import UnifiedNetwork
 from dataset import UnifiedPoseDataset
 from visualize import UnifiedVisualization
 
-training_dataset = UnifiedPoseDataset(mode='train', loadit=True, name='test2')
+training_dataset = UnifiedPoseDataset(mode='test', loadit=True, name='test2')
 training_dataset[0]
 print training_dataset.samples[0]
 training_dataloader = torch.utils.data.DataLoader(training_dataset, batch_size = 1, shuffle=False, num_workers=1)
@@ -51,6 +51,7 @@ with torch.no_grad():
         hand_cell_counter += int(true_hand_cell == pred_hand_cell)
         object_cell_counter += int(true_object_cell == pred_object_cell)
         
+        print hand_cell_counter, object_cell_counter
         z, v, u = true_hand_cell[1:]
         dels = pred_hand_pose[0,:,z, v, u].reshape(21, 3)
         del_u, del_v, del_z = dels[:,0], dels[:,1], dels[:,2]
@@ -62,9 +63,14 @@ with torch.no_grad():
         object_points = training_dataset.target_to_control(del_u, del_v, del_z, (u, v, z))
 
 
-        print training_dataset.samples[batch]
+        # print training_dataset.samples[batch]
 
+        """
         viz = UnifiedVisualization()
         viz.plot_hand(hand_points)
         viz.plot_box(object_points[1:9, :])
         viz.plot()
+
+        """
+    print hand_cell_counter * 1. / batch
+    print object_cell_counter * 1. / batch
