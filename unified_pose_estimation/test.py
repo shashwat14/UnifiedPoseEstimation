eb0e8081e7f5d1c6e82fc25f259c257fc0e65fe3
shashwatverma14@gmail.com <shashwatverma14@gmail.com>
2019-06-23 08:04:50 -0400

    add performance metrics

--- unified_pose_estimation/test.py
+++ unified_pose_estimation/test.py
@@ -11,7 +11,7 @@ from visualize import UnifiedVisualization
 training_dataset = UnifiedPoseDataset(mode='test', loadit=True, name='test2')
 training_dataset[0]
 print training_dataset.samples[0]
-training_dataloader = torch.utils.data.DataLoader(training_dataset, batch_size = 1, shuffle=False, num_workers=1)
+training_dataloader = torch.utils.data.DataLoader(training_dataset, batch_size = 1, shuffle=True, num_workers=1)
 
 model = UnifiedNetwork()
 model.load_state_dict(torch.load('../models/unified_net.pth'))
@@ -28,6 +28,9 @@ with torch.no_grad():
     object_cell_counter = 0.
     object_counter = 0.
     action_counter = 0.
+
+    hand_detected = False
+    object_detected = False
 
     for batch, data in enumerate(tqdm(training_dataloader)):
     
@@ -48,20 +51,29 @@ with torch.no_grad():
         pred_hand_cell = np.unravel_index(pred_hand_conf.argmax(), pred_hand_conf.shape)
         pred_object_cell = np.unravel_index(pred_object_conf.argmax(), pred_object_conf.shape)
         
+        # hand cell correctly detected
         hand_cell_counter += int(true_hand_cell == pred_hand_cell)
+        hand_detected = true_hand_cell == pred_hand_cell
+
+        # object cell correctly detected
         object_cell_counter += int(true_object_cell == pred_object_cell)
-        
-        print hand_cell_counter, object_cell_counter
+        object_detected = true_object_cell == pred_object_cell
+
         z, v, u = true_hand_cell[1:]
         dels = pred_hand_pose[0,:,z, v, u].reshape(21, 3)
         del_u, del_v, del_z = dels[:,0], dels[:,1], dels[:,2]
         hand_points = training_dataset.target_to_control(del_u, del_v, del_z, (u, v, z))
+        
+        if hand_detected:
+            action_counter += int(np.argmax(pred_action_prob[0, :, z, v, u]) == true_action_prob[0, z, v, u])
 
         z, v, u = true_object_cell[1:]
         dels = pred_object_pose[0,:,z, v, u].reshape(21, 3)
         del_u, del_v, del_z = dels[:,0], dels[:,1], dels[:,2]
         object_points = training_dataset.target_to_control(del_u, del_v, del_z, (u, v, z))
 
+        if object_detected:
+            object_counter += int(np.argmax(pred_object_prob[0, :, z, v, u]) == true_object_prob[0, z, v, u])
 
         # print training_dataset.samples[batch]
 
@@ -72,5 +84,9 @@ with torch.no_grad():
         viz.plot()
 
         """
+
     print hand_cell_counter * 1. / batch
     print object_cell_counter * 1. / batch
+    print action_counter * 1 / hand_cell_counter
+    print object_counter * 1 / object_cell_counter
+    
